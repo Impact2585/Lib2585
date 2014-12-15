07130e9a3cee4e2bddd5b4263b775bd13d2810c2
LoadingPleaseWait <michael@murphey.org>
2014-12-14 22:29:12 -0600

    added environment, executer, initializable

---
+++ src/org/_2585robophiles/lib2585/Executer.java
@@ -0,0 +1,10 @@
+package org._2585robophiles.lib2585;
+
+public interface Executer extends Initializable{
+	
+	/**
+	 * Execute actions which should happen every 20 or so milliseconds
+	 */
+	public void execute();
+	
+}
---
+++ src/org/_2585robophiles/lib2585/Initializable.java
@@ -0,0 +1,11 @@
+package org._2585robophiles.lib2585;
+
+public interface Initializable {
+
+	/**
+	 * initialize object with beginning state
+	 * @param environment environment of the systems
+	 */
+	public void init(Environment2585 environment);
+	
+}
