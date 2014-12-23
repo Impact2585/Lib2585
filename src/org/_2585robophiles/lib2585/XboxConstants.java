1979939c831e44b889a912ae7e36bf6af378898f
Killin-A13 <amanuelbayu@gmail.com>
2014-12-23 13:44:43 -0600

    xbox constants

---
+++ src/org/_2585robophiles/lib2585/XboxConstants.java
@@ -0,0 +1,36 @@
+package org._2585robophiles.lib2585;
+
+/*
+
+1: A
+2: B
+3: X
+4: Y
+5: Left Bumper
+6: Right Bumper
+7: Back
+8: Start
+9: Left Joystick
+10: Right Joystick
+
+*/
+
+public interface XboxConstants {
+	
+	public static final int LEFT_X_AXIS = 1;
+	public static final int LEFT_Y_AXIS = 2;
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
+
+	public static final int A_BUTTON = 1;
+	public static final int B_BUTTON = 2;
+	public static final int X_BUTTON = 3;
+	public static final int Y_BUTTON = 4;
+	public static final int LEFT_BUMPER = 5;
+	public static final int RIGHT_BUMPER = 6;
+	public static final int BACK_BUTTON = 7;
+	public static final int START_BUTTON = 8;
+	public static final int LEFT_JOYSTICK_BUTTON = 9;
+	public static final int RIGHT_JOYSTICK_BUTTON = 10;
+	
+}
