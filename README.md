e85636c18e77304235c5763b4df25979c6d6c0be
LoadingPleaseWait <michael@murphey.org>
2014-12-24 20:46:08 -0600

    format ExecuterBasedRobot, edit README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Lib2585
 =======
 
 This FRC library was made from the [code for aerbot](https://github.com/2585Robophiles/aerbot-champs), team 2585's robot for 2014, which has [unit tests](https://github.com/2585Robophiles/aerbot-junit).
+
+Downloads can be found on the [releases](https://github.com/2585Robophiles/Lib2585/releases) page.
+
+Lib2585 is dual licensed under the [GPL v3](http://www.gnu.org/licenses) and BSD License for WPILib.
+Lib2585 is free software: you can redistribute it and/or modify it under the terms of those two licenses.
--- src/org/_2585robophiles/lib2585/ExecuterBasedRobot.java
+++ src/org/_2585robophiles/lib2585/ExecuterBasedRobot.java
@@ -3,52 +3,57 @@ package org._2585robophiles.lib2585;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
- * Robot that uses executers
- * This is the equivelant of a main class in WPILib.
+ * Robot that uses executers This is the equivelant of a main class in WPILib.
  */
 public abstract class ExecuterBasedRobot extends IterativeRobot {
 
-    private Executer executer;
+	private Executer executer;
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#robotInit()
-     */
-    public abstract void robotInit();
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#robotInit()
+	 */
+	public abstract void robotInit();
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#autonomousPeriodic()
-     */
-    public void autonomousPeriodic() {
-        executer.execute();
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#autonomousPeriodic()
+	 */
+	public void autonomousPeriodic() {
+		executer.execute();
+	}
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#teleopPeriodic()
-     */
-    public void teleopPeriodic() {
-        executer.execute();
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#teleopPeriodic()
+	 */
+	public void teleopPeriodic() {
+		executer.execute();
+	}
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#testPeriodic()
-     */
-    public void testPeriodic() {
-        executer.execute();
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#testPeriodic()
+	 */
+	public void testPeriodic() {
+		executer.execute();
+	}
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#disabledInit()
-     */
-    public void disabledInit() {
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#disabledInit()
+	 */
+	public void disabledInit() {
 
-    }
+	}
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#disabledPeriodic()
-     */
-    public void disabledPeriodic() {
+	/*
+	 * (non-Javadoc)
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#disabledPeriodic()
+	 */
+	public void disabledPeriodic() {
 
-    }
+	}
 
 	/**
 	 * @return the executer
