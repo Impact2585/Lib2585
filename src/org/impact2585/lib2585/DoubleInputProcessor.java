ed67b4e237f64c66e169ca049a0d22269e363b8d
J0hnCena <joshua99wong@gmail.com>
2016-04-21 06:55:43 -0500

    Change general InputProcessor to DoubleInputProcessor and BooleanInputProcessor

--- src/org/impact2585/lib2585/DoubleInputProcessor.java
+++ src/org/impact2585/lib2585/DoubleInputProcessor.java
@@ -1,14 +1,16 @@
 package org.impact2585.lib2585;
 
+
 /**
  * Functional interface that contains a method to process input
  */
 @FunctionalInterface
-public interface InputProcessor<T>{
+public interface DoubleInputProcessor{
 	
 	/**
-	 * @param input an array of input values from a controller
+	 * @param input an array of double input values from a controller
 	 * @return the value(usually speed) that should be sent to the motor controllers 
 	 */
-	public double process(T... input);
+	public double process(Double... input);
+	
 }
